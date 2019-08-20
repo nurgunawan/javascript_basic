0d119354d261f2af8cf3dce8eab1a62d383899d1
nurgunawan <nurgunawan@gmail.com>
2019-08-20 13:53:27 +0700

    add filearray

---
+++ array.js
@@ -0,0 +1,15 @@
+const book = ['progamming','networking','English'];
+
+
+console.log(book);
+
+//add book to array
+
+book[3] = 'Comic';
+
+console.log(book);
+
+
+//length of array after add new book
+
+console.log(book.length);
