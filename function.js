f811530b6c82653f813cf66526210f0f2c79225a
nurgunawan <nurgunawan@gmail.com>
2019-08-20 15:02:19 +0700

    add file function

---
+++ function.js
@@ -0,0 +1,20 @@
+//function hello
+
+function hello(name) {
+    console.log('Halo ' + name);
+
+}
+
+hello('nur');
+hello('gunawan');
+
+//function Ccalculate number
+
+function calculateNumber(number1,number2){
+    
+    return number1 + number2;
+
+}
+
+const result = calculateNumber(5,6);
+console.log (result);
